6fe55e83117bd43fe5002e27855b71753bb73cf7
Jonathon Merz <1848149+jmmerz@users.noreply.github.com>
2020-05-14 16:44:32 -0700

    Version 1.1 of gmail-search-text-to-page-title.

--- gmail-search-text-to-page-title/gmail-search-text-to-page-title.user.js
+++ gmail-search-text-to-page-title/gmail-search-text-to-page-title.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GMail - Search Text to Page Title
 // @namespace    https://github.com/jmmerz/user-scripts
-// @version      1.0
+// @version      1.1
 // @description  Add the text of a search to GMail page title to make browser history more useful
 // @author       jmmerz
 // @match        https://mail.google.com/mail/u/*
@@ -10,6 +10,10 @@
 
 /* ===========================================================
  * Release History:
+ * 1.1 (2020-05-14)
+ *   - Update method for finding search input fields.
+ *   - Add ability to set title text when loading GMail with a URL containing a search
+ *
  * 1.0 (2020-04-13)
  *   - Initial release
  * =========================================================== */
@@ -39,31 +43,40 @@
             }
             return;
         } else {
+            /** Function to set the document title iff we are on a search output page */
+            let setDocumentSearchTitle = function(searchText) {
+                if(document.title.startsWith("Search results - ")) {
+                    document.title = document.title.replace(" - ", ": '" + searchText.trim() + "' - ");
+                }
+            };
+
             // Install mutation observer for document.title
             var docTitleTarget = document.querySelector('title');
             var titleObserver = new MutationObserver(function(mutations) {
-                if(document.title.startsWith("Search results - ")) {
-                    document.title = document.title.replace(" - ", ": '" + SearchTitle.mostRecentSearchText + "' - ");
-                }
+                setDocumentSearchTitle(SearchTitle.mostRecentSearchText);
             });
             var config = {
                 childList: true,
             };
             titleObserver.observe(docTitleTarget, config);
 
-            var keydownEventListener = function(ev) {
-                if(ev.keyCode == 13) { // <Enter>
-                    SearchTitle.mostRecentSearchText = ev.target.value;
+            var keydownEventListener = function(evt) {
+                if(evt.keyCode == 13) { // <Enter>
+                    SearchTitle.mostRecentSearchText = evt.target.value;
                 }
             };
 
             // Install event listener on form.
-            // TODO: Could potentially just query the form data from the document title MutationObserver
-            var inputFields = searchForm.getElementsByClassName("gb_jf");
-            for(var i = 0; i < inputFields.length; i++) {
-                if(inputFields[i].nodeName == "INPUT") {
-                    //inputFields[i].addEventListener('keypress', keypressEventListener, false);
-                    inputFields[i].addEventListener('keydown', keydownEventListener, false);
+            // TODO: Could potentially just query the form data from inside of the document title MutationObserver
+            //var inputFields = searchForm.getElementsByClassName("gb_nf");
+            var inputFields = searchForm.getElementsByTagName("input");
+            for(let inputField of inputFields) {
+                inputField.addEventListener('keydown', keydownEventListener, false);
+
+                // In case the page being loaded already has a search, enter that:
+                if(inputField.value !== null && inputField.value !== '') {
+                    SearchTitle.mostRecentSearchText = inputField.value;
+                    setDocumentSearchTitle(SearchTitle.mostRecentSearchText);
                 }
             }
 
